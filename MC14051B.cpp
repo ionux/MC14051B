7b67f2ae8990579025db83e7b0f7058bd7c26817
pbrown66 <pbrown@mykolab.com>
2015-11-29 18:29:12 +0100

    Correcting copyleft

--- MC14051B.cpp
+++ MC14051B.cpp
@@ -1,5 +1,5 @@
 /*
-Copyleft 2015 Paul Cambra Brwon.
+Copyleft 2015 Paul Cambra Brown.
 
 This program is free software: you can redistribute it and/or
 modify it under the terms of the GNU General Public License as
